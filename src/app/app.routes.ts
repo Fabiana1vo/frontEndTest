24c1671268943bd4828999cd7a991dc9511a8128
Fabiana1vo <fabianasouzaivo@icloud.com>
2024-05-10 14:27:45 -0300

    feat: realizei a implementação de uma tela de login.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { ContainerComponent } from './components/core/container/container.component';
 import { ListaPersonagemComponent } from './components/layout/lista-personagem/lista-personagem.component';
+import { LoginComponent } from './components/layout/login/login.component';
 
 export const routes: Routes = [
   {path: '', component: ContainerComponent, children: [
     {path: '', redirectTo: '/home', pathMatch: 'full'},
     {path:'home', component: ListaPersonagemComponent }
-  ]}
- ];
+  ]},
+  {path:'login', component: LoginComponent },
+
+
+];
 
--- src/app/components/layout/login/login.component.ts
+++ src/app/components/layout/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  visibilityIcon = "assets/icons/eyeOpen.svg";
+  altIcon = "Icone para visualizar";
+  visibilityType = 'password';
 
+  protected formBuilderService = inject(FormBuilder);
+
+
+
+  protected formLogin = this.formBuilderService.group({
+    email:['abc@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456',[Validators.required]]
+
+  })
+
+
+  protected logar(){
+
+    alert("Login efetuado com sucesso!")
+  }
+
+  protected changeVisibility(){
+      console.log("Mudei a visibilidade");
+      if(this.visibilityType == 'password'){
+        this.visibilityType = 'text'
+        this.visibilityIcon = "assets/icons/eyeClose.svg";
+      } else {
+        this.visibilityType = 'password'
+        this.visibilityIcon = "assets/icons/eyeOpen.svg";
+      }
+  }
 }
